668659cfb184b4c3ea8970436a55fed6b0808991
duncanjpark <97486467+duncanjpark@users.noreply.github.com>
2022-04-30 18:55:20 -0400

    Update betting.py

--- scripts/betting.py
+++ scripts/betting.py
@@ -14,25 +14,6 @@ Function definitions for operations on the bets.py and portfolio.py classes
     # Parameters: a league (constant)
     
     
-# Display all events
-    # Returns: void
-    # Parameters: dataframe of events
-    
-    
-# Display displayGroups for an event:
-    # Returns: void
-    # Parameters: event
-
-    
-# Display markets for a displayGroup:
-    # Returns: void
-    # Parameters: displayGroup
-    
-    
-# Display outcomes for a market:
-    # Returns: void
-    # Parameters: market
-    
     
 # Place bet
     # Returns: confirmation of bet placed
