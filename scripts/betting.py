a902c2cc194192f6e6584258c72d2bcbb7f26819
duncanjpark <97486467+duncanjpark@users.noreply.github.com>
2022-04-30 18:52:58 -0400

    Update and rename response.py to betting.py

---
+++ scripts/betting.py
@@ -0,0 +1,42 @@
+"""
+
+Function definitions for operations on the bets.py and portfolio.py classes
+
+"""
+
+# Constants for URLs
+
+
+
+
+# Function to initialize events for a given league
+    # Returns: dataframe of events with their start times and if they're live
+    # Parameters: a league (constant)
+    
+    
+# Display all events
+    # Returns: void
+    # Parameters: dataframe of events
+    
+    
+# Display displayGroups for an event:
+    # Returns: void
+    # Parameters: event
+
+    
+# Display markets for a displayGroup:
+    # Returns: void
+    # Parameters: displayGroup
+    
+    
+# Display outcomes for a market:
+    # Returns: void
+    # Parameters: market
+    
+    
+# Place bet
+    # Returns: confirmation of bet placed
+    # Parameters: outcome object, stake, portfolio object
+
+    
+    
