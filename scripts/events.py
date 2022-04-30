cae5c2747e5bbfadd79823a9d3a10fbe286dac20
duncanjpark <97486467+duncanjpark@users.noreply.github.com>
2022-04-30 18:36:12 -0400

    Create events.py

---
+++ scripts/events.py
@@ -0,0 +1,46 @@
+"""
+
+Class definitions for event, displayGroup, market, and outcome
+
+"""
+
+
+# event class
+    # has descriptive variables such as time, competititors, and link
+    # has displayGroup objects
+    
+    # needs initialization function to take link as argument and fill variables
+        # which includes creating displayGroup objects
+
+        
+    
+
+# displayGroup class
+    # has description and type of bets
+    # has market objects
+    
+    # needs initialization function to take json argument and fill variables
+        # which includes creating market objects
+
+
+        
+        
+# market class
+    # has variables such as id, description, and period
+    # has outcome objects
+    
+    # needs initialization function to take json argument and fill variables
+        # which includes creating outcome objects
+
+
+        
+        
+# outcome class
+    # has outcome type, id, description etc.
+    # and most importantly, the price (odds)
+    # from price create implied odds variable
+    
+    # needs initialization function to take json argument and fill variables
+        # which includes calculating implied odds
+
+
