66143da648e9d137b49f28d5e35209efbd4f3930
duncanjpark <97486467+duncanjpark@users.noreply.github.com>
2022-04-30 18:54:55 -0400

    Create portfolio.py

---
+++ scripts/portfolio.py
@@ -0,0 +1,22 @@
+"""
+
+Class definition for portfolio/account of bets
+
+"""
+
+# portfolio class
+    # has dataframe of all placed bets and their timing
+
+    # Display all current placed bets
+        # Returns: void
+        # Parameters: portfolio object
+
+
+    # Display transaction history    
+        # Returns: void
+        # Parameters: portfolio object
+
+
+    # Display portfolio performance
+        # Returns: void
+        # Parameters: portfolio object
