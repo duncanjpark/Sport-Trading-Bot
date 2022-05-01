853cf5d7b220f0436e680be1a34ed4cd80578459
Duncan Park <dpark6@nd.edu>
2022-05-01 02:28:36 -0400

    got json requests working, bot can print out outcomes and odds for specified bets (temporarily hardcoded)

--- scripts/betting.py
+++ scripts/betting.py
@@ -7,6 +7,8 @@ Function definitions for operations on the bets.py and portfolio.py classes
 """
 import pandas as pd
 import requests
+from IPython.display import display
+
 
 # Constants for URLs
 path = "https://www.bovada.lv/services/sports/event/coupon/events/A/description"
@@ -21,9 +23,13 @@ def get_events(league):
     # Returns: dataframe of events with their start times and if they're live
     # Parameters: a league (constant)
     data = requests.get(URLs[league]).json()
-    data = data[0]["events"][0]
 
-    print(data)
+    # Flatten data
+    events = pd.json_normalize(data, record_path=['events'])
+
+    # return dataframe of events
+    return events
+
 
 # Place bet
     # Returns: confirmation of bet placed
@@ -32,3 +38,29 @@ def get_events(league):
 # Display all events
     # Returns: void
     # Parameters: dataframe of events
+
+
+def display_events(events):
+    events['startTime'] += -14400000    # adjust to Eastern Timezone
+    events['startTime'] = pd.to_datetime(
+        events['startTime'], unit='ms', origin='unix')  # convert formatting
+    display(events[['description', 'startTime']])   # display events and time
+
+
+def display_outcomes(market):
+    outcomes = market['outcomes'][0]
+    print("Market:     " + market['description'][0] + "\n")
+    impliedTotal = 0
+    for outcome in outcomes:
+        print("Outcome:     " + outcome['description'])
+        print("Odds:     Decimal:  " + outcome['price']['decimal']
+              + "  American:  " + outcome['price']['american'])
+        print("Implied Odds:     %4.4f\n" %
+              implied(outcome['price']['decimal']))
+        impliedTotal += implied(outcome['price']['decimal'])
+    print("Cumulative sum of implied odds:     %4.4f" % impliedTotal)
+    print("House Edge:     %4.4fs%%" % ((impliedTotal - 1)*100))
+
+
+def implied(odds):
+    return (1 / float(odds))
--- scripts/test_betting.py
+++ scripts/test_betting.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
-from betting import *
+from betting import display_events, get_events, display_outcomes
+from IPython.display import display
+import pandas as pd
 
 
-get_events("NBA")
+events = get_events("NBA")
+
+print('\n')
+
+display_events(events)
+
+event = events.iloc[[1]]
+
+dispGroups = event['displayGroups'][1]
+
+displayGroup = dispGroups[0]
+markets = displayGroup['markets']
+markets = pd.DataFrame(markets)
+
+market = markets.iloc[[0]]
+
+print('\n\n')
+
+
+display_outcomes(market)
