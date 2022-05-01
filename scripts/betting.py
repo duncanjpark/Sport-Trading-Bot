96ce721bd82419988d5b824bf4065c411063e6da
Duncan Park <dpark6@nd.edu>
2022-05-01 00:21:24 -0400

    testing out getting events

--- scripts/betting.py
+++ scripts/betting.py
@@ -5,23 +5,30 @@
 Function definitions for operations on the bets.py and portfolio.py classes
 
 """
+import pandas as pd
+import requests
 
 # Constants for URLs
-
-
+path = "https://www.bovada.lv/services/sports/event/coupon/events/A/description"
+URLs = {
+    "NBA": path + "/basketball/nba",
+    "NFL": path + "/description/football/nfl"
+}
 
 
 # Function to initialize events for a given league
+def get_events(league):
     # Returns: dataframe of events with their start times and if they're live
     # Parameters: a league (constant)
-    
-    
-    
+    data = requests.get(URLs[league]).json()
+    data = data[1]["events"]
+
+    print(data)
+
 # Place bet
     # Returns: confirmation of bet placed
     # Parameters: outcome object, stake, portfolio object
 
-    
 # Display all events
-        # Returns: void
-        # Parameters: dataframe of events
+    # Returns: void
+    # Parameters: dataframe of events
---
+++ scripts/test_betting.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from betting import *
+
+
+get_events("NBA")
