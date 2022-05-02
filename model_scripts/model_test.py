b26f5a8d93d93fc63c5591a097d7c9ac0712546c
Duncan Park <dpark6@nd.edu>
2022-05-01 22:06:33 -0400

    more model exploration and progress

--- model_scripts/model_test.py
+++ model_scripts/model_test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import statsmodels.formula.api as smf
-import sys
 import pandas as pd
 from IPython.display import display
-
+import numpy as np
+from matplotlib import pyplot as plt
 
 # sys.path.insert(0, '../data')
 
@@ -14,7 +14,7 @@ types = {
     'Hitters': 'str',
     "H-AB": 'str',
     'AB': 'str',
-    'R': 'str',
+    'R': 'int',
     'H': 'str',
     'RBI': 'str',
     'BB': 'str',
@@ -29,24 +29,42 @@ types = {
     'Hitter Id': 'str'
 }
 
-initial_DF = pd.read_csv('../data/hittersByGame.csv', dtype=types)
+
+initial_DF = pd.read_csv('../data/hittersByGame.csv',
+                         header=0, names=types.keys())
+
+for i, col in enumerate(types.keys()):
+    if i > 1 and i < 12:
+        initial_DF[col] = pd.to_numeric(initial_DF[col], errors='coerce')
+
+
+display(initial_DF)
 
 initial_DF = initial_DF[['Hitters', 'R', 'AB', 'Game']]
-display(initial_DF)
-initial_DF = initial_DF.dropna(how='all')
-"""
-filter1 = initial_DF["R"] != "--"
-filter2 = initial_DF["R"] != "NaN"
-initial_DF.where(filter1, inplace=True)
-
-initial_DF.astype({'R': 'float', 'AB': 'float'}).dtypes
-"""
-initial_DF.reset_index()
-display(initial_DF)
 
 mlb_data = initial_DF
 
 mlb_data.info()
 
+print('\n')
+
 model = smf.ols('R ~ AB', data=mlb_data)
-model = model.fit()
+results = model.fit()
+
+print(results.params)
+print(results.tvalues)
+print(results.t_test([1, 0]))
+print(results.f_test(np.identity(2)))
+
+run_predict = results.predict()
+
+# Plot regression against actual data
+plt.figure(figsize=(12, 6))
+# scatter plot showing actual data
+plt.plot(mlb_data['AB'], mlb_data['R'], 'o')
+plt.plot(mlb_data['AB'], run_predict, 'r', linewidth=2)   # regression line
+plt.xlabel('AB')
+plt.ylabel('Runs')
+plt.title('AB vs Runs')
+
+plt.show()
